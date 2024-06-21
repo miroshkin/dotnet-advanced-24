0ad82eb2b7792e1df9deb90d2e8bb6441eddbef8
Anton Miroshkin <Anton_Miroshkin@epam.com>
2024-06-21 12:38:08 +0200

    Add identity client app

---
+++ 05 security authentication authorization/IdentityClient/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdentityClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
